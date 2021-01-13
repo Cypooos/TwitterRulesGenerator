05bf3b35454be7b0fab26adee9f0bcd19d3f78fe
Cypooos <60020572+Cypooos@users.noreply.github.com>
2021-01-13 22:56:07 +0100

    Adding default elements

---
+++ twitter_credentials.py
@@ -0,0 +1,6 @@
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = "1185652900141371392-FpbkXi1G5AvJmvX05WbZ1g1YCMBrhD"
+ACCESS_TOKEN_SECRET = "FFqhl6xynLnzhNHc2zsmCmt0YOtdl9OPSVUIAE4WQSBqi"
+CONSUMER_KEY = "pO6BIWeAhNupuXmjbT1tORfMm"
+CONSUMER_SECRET = "TYwzOf9AotgNngh9qVFdYXaMsp6LN2KVB22MEK9Oa3KD6W6o90"
