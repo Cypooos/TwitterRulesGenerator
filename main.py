b3d1ef6f4fee202f984aeca101a32511e5b91220
Cypooos <60020572+Cypooos@users.noreply.github.com>
2022-06-05 15:51:12 +0200

    idk

--- main.py
+++ main.py
@@ -18,12 +18,17 @@ last_one = None
 while True:
     rule_raw = g_r.model_generator.one_rule(255,last_one)
     rule_utf = str(rule_raw.numpy(), encoding='utf-8', errors = 'ignore')
-    rule = rule_utf.split("\r\n")[1].replace(":newline:","\n")
+    rule_splitted = rule_utf.split("\r\n")
+    rule = rule_splitted[1].replace(":newline:","\n")
+    last_one = rule_splitted[1]+"\r\n"
 
     rule_ano = re.sub(r'^[^#]*#[0-9]{4}', '', rule).strip()
     print("New Rule in 20 seconds:", rule_ano)
-    sleep(5)
-    # t = api.update_status(rule_ano)
+    sleep(20)
+    try:
+        t = api.update_status(rule_ano)
+    except Exception as e:
+        print(e)
 
 
-    #sleep(580)
+    sleep(580)
